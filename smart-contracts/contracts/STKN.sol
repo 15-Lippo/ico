08e0916d9ca3b611bda6ffc7ef7a955f89d9a004
Lisprocoin <83799681+15-Lippo@users.noreply.github.com>
2023-03-09 01:23:01 +0100

    Update STKN.sol

--- smart-contracts/contracts/STKN.sol
+++ smart-contracts/contracts/STKN.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.16;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract STKN is ERC20 {
-    constructor() ERC20("Staking Token", "STKN") {
-        _mint(msg.sender, 10000000 * 1e18);
+    constructor() ERC20("Lisprocoin", "LSP20") {
+        _mint(msg.sender, 10000000000 * 1e18);
     }
 }
