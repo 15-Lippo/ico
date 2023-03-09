3d5c28420200eae2bcefeb1fcedc5777cd0c9207
Lisprocoin <83799681+15-Lippo@users.noreply.github.com>
2023-03-09 01:18:39 +0100

    Update constants.js

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import stknICO from "./stknICO.json";
 
-export const stakingICOAddress = "0x8f0680d7aa5a919eefcae59f79956495427217cc";
-export const stakingICOAbi = stknICO.abi;
+export const stakingICOAddress = "0x70E546c7a2cA4495cFcbE263a3b6D5ce68B2204C";
+export const stakingICOAbi = LSP20 ICO.abi;
