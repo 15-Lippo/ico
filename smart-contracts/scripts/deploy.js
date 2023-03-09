713f7c30400449ed650fcaf48498e593e2cb5b30
Lisprocoin <83799681+15-Lippo@users.noreply.github.com>
2023-03-09 01:27:17 +0100

    Update deploy.js

--- smart-contracts/scripts/deploy.js
+++ smart-contracts/scripts/deploy.js
@@ -1,17 +1,16 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-  //STKN
-  console.log("Deploying STKN Contract...");
-  const STKNFactory = await ethers.getContractFactory("STKN");
-  const stkn = await STKNFactory.deploy();
+  //LSP20 
+  console.log("Deploying LSP20 Contract...");
+  const Lsp20Factory = await ethers.getContractFactory("LSP20");
+  const lsp20 = await LSP20 factory.deploy();
 
-  console.log("Deployed STKN:", stkn.address);
-
-  //STKNICO
-  console.log("Deploying stknICO Contract...");
-  const StknICOFactory = await ethers.getContractFactory("StknICO");
-  const stknICO = await StknICOFactory.deploy(
+  console.log("Deployed LSP20:", Lsp20.address)
+  //LSP20 ICO
+  console.log("Deploying Lsp20ICO Contract...");
+  const StknICOFactory = await ethers.getContractFactory("lsp20ICO");
+  const lsp20 ICO = await lsp20ICOFactory.deploy(
     "0x00f2a05f8327ac26e1994b92dbd4e4813bfa8609",
     stkn.address
   );
